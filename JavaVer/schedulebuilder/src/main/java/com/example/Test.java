aec88c51d5e0bb98037bc6a0901b946bcb9031bc
JadynF <jrf038@email.latech.edu>
2023-06-04 23:05:21 -0500

    added java scraping using requests

--- JavaVer/schedulebuilder/src/main/java/com/example/Test.java
+++ JavaVer/schedulebuilder/src/main/java/com/example/Test.java
@@ -27,39 +27,39 @@ public class Test {
     public static void main( String[] args ) {
 
         try {
-        HttpGet request = new HttpGet("https://boss.latech.edu/ia-bin/tsrvweb.cgi?&WID=W&WID&ConfigName=rcrssecthp1&ReqNum=1&TransactionSource=H&tserve_trans_config=rcrssecthp1.cfg&tserve_host_code=HostZero&tserve_tiphost_code=TipZero#top");
-        CloseableHttpResponse response = httpClient.execute(request);
-        response.close();
+            HttpGet request = new HttpGet("https://boss.latech.edu/ia-bin/tsrvweb.cgi?&WID=W&WID&ConfigName=rcrssecthp1&ReqNum=1&TransactionSource=H&tserve_trans_config=rcrssecthp1.cfg&tserve_host_code=HostZero&tserve_tiphost_code=TipZero#top");
+            CloseableHttpResponse getResponse = httpClient.execute(request);
+            getResponse.close();
 
-        String postUrl = "https://boss.latech.edu/ia-bin/tsrvweb.cgi";
+            String postUrl = "https://boss.latech.edu/ia-bin/tsrvweb.cgi";
 
-        HttpPost postTerms = new HttpPost(postUrl);
-        List<NameValuePair> urlTermParams = new ArrayList<>();
-        urlTermParams.add(new BasicNameValuePair("tserve_host_code", "HostZero"));
-        urlTermParams.add(new BasicNameValuePair("tserve_tiphost_code", "TipZero"));
-        urlTermParams.add(new BasicNameValuePair("tserve_trans_config", "RCRSSECTHP1.cfg"));
-        urlTermParams.add(new BasicNameValuePair("tserve_tip_write", "%7C%7CWID%7CSID%7CPIN%7CTerm%7CSubject%7CCourseID%7CAppTerm%7CConfigName"));
-        urlTermParams.add(new BasicNameValuePair("TransactionSource", "H"));
-        urlTermParams.add(new BasicNameValuePair("Term", "20241"));
-        postTerms.setEntity(new UrlEncodedFormEntity(urlTermParams));
-        CloseableHttpResponse response2 = httpClient.execute(postTerms);
-        response2.close();
+            HttpPost postTerms = new HttpPost(postUrl);
+            List<NameValuePair> urlTermParams = new ArrayList<>();
+            urlTermParams.add(new BasicNameValuePair("tserve_host_code", "HostZero"));
+            urlTermParams.add(new BasicNameValuePair("tserve_tiphost_code", "TipZero"));
+            urlTermParams.add(new BasicNameValuePair("tserve_trans_config", "RCRSSECTHP1.cfg"));
+            urlTermParams.add(new BasicNameValuePair("tserve_tip_write", "%7C%7CWID%7CSID%7CPIN%7CTerm%7CSubject%7CCourseID%7CAppTerm%7CConfigName"));
+            urlTermParams.add(new BasicNameValuePair("TransactionSource", "H"));
+            urlTermParams.add(new BasicNameValuePair("Term", "20241"));
+            postTerms.setEntity(new UrlEncodedFormEntity(urlTermParams));
+            CloseableHttpResponse termResponse = httpClient.execute(postTerms);
+            termResponse.close();
 
-        HttpPost postSubject = new HttpPost(postUrl);
-        List<NameValuePair> urlSubjectParams = new ArrayList<>();
-        urlSubjectParams.add(new BasicNameValuePair("tserve_tip_read_destroy", ""));
-        urlSubjectParams.add(new BasicNameValuePair("tserve_host_code", "HostZero"));
-        urlSubjectParams.add(new BasicNameValuePair("tserve_tiphost_code", "TipZero"));
-        urlSubjectParams.add(new BasicNameValuePair("tserve_trans_config", "rcrssecthp2.cfg"));
-        urlSubjectParams.add(new BasicNameValuePair("tserve_tip_write", "%7C%7CWID%7CSID%7CPIN%7CTerm%7CSubject%7CCourseID%7CAppTerm%7CConfigName"));
-        urlSubjectParams.add(new BasicNameValuePair("TransactionSource", "H"));
-        urlSubjectParams.add(new BasicNameValuePair("Subject", "CSC"));
-        urlSubjectParams.add(new BasicNameValuePair("ReqNum", "2"));
-        postSubject.setEntity(new UrlEncodedFormEntity(urlSubjectParams));
-        CloseableHttpResponse response3 = httpClient.execute(postSubject);
+            HttpPost postSubject = new HttpPost(postUrl);
+            List<NameValuePair> urlSubjectParams = new ArrayList<>();
+            urlSubjectParams.add(new BasicNameValuePair("tserve_tip_read_destroy", ""));
+            urlSubjectParams.add(new BasicNameValuePair("tserve_host_code", "HostZero"));
+            urlSubjectParams.add(new BasicNameValuePair("tserve_tiphost_code", "TipZero"));
+            urlSubjectParams.add(new BasicNameValuePair("tserve_trans_config", "rcrssecthp2.cfg"));
+            urlSubjectParams.add(new BasicNameValuePair("tserve_tip_write", "%7C%7CWID%7CSID%7CPIN%7CTerm%7CSubject%7CCourseID%7CAppTerm%7CConfigName"));
+            urlSubjectParams.add(new BasicNameValuePair("TransactionSource", "H"));
+            urlSubjectParams.add(new BasicNameValuePair("Subject", "CSC"));
+            urlSubjectParams.add(new BasicNameValuePair("ReqNum", "2"));
+            postSubject.setEntity(new UrlEncodedFormEntity(urlSubjectParams));
+            CloseableHttpResponse subjectResponse = httpClient.execute(postSubject);
 
-        System.out.println(EntityUtils.toString(response3.getEntity()));
-        response3.close();
+            System.out.println(EntityUtils.toString(subjectResponse.getEntity()));
+            subjectResponse.close();
 
         }
         catch (Exception e) {
